852b3896f9b923ac2f0111e008eca1a5fc8c7204
Donna <skies09@hotmail.com>
2018-10-01 14:40:37 +0000

    addition of map

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 53.406564, lng: -2.555394 },
+        { lat: 53.406973, lng: -2.967295},
+        { lat: 52.024537, lng: -0.709274}
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
